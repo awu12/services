1c9875754b038a23fc82b940fa044778bde7797e
awu12 <tempatwisatabaru@gmail.com>
2019-06-11 06:36:56 +0700

    Update random.js

--- random.js
+++ random.js
@@ -19,13 +19,18 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*10)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2019/01/erp-software-for-small-companies-you.html"}
 if(x=="2"){xxx="/2019/01/what-is-best-erp-system-based-on.html"}
 if(x=="3"){xxx="/2019/01/erp-software-for-small-manufacturers-to.html"}
 if(x=="4"){xxx="/2019/01/what-is-erp-in-manufacturing.html"}
-if(x=="5"){xxx="/2019/01/which-is-best-erp-software-for-your.html"}a_to_ve[i].href="http://erp-software-services.review"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2019/06/the-best-enterprise-erp-software.html"}
+if(x=="6"){xxx="/2019/06/how-to-choose-erp-software-for-midsize.html"}
+if(x=="7"){xxx="/2019/06/the-biggest-erp-software-companies.html"}
+if(x=="8"){xxx="/2019/06/what-does-erp-software-mean-for-your.html"}
+if(x=="9"){xxx="/2019/03/the-benefits-of-erp-for-small-business.html"}
+if(x=="10"){xxx="/2019/01/which-is-best-erp-software-for-your.html"}a_to_ve[i].href="http://erp-software-services.review"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
