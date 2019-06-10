845455c75c4f38a4901786391cbe534efd1defc3
awu12 <tempatwisatabaru@gmail.com>
2019-06-11 06:45:51 +0700

    Update random.js

--- random.js
+++ random.js
@@ -30,7 +30,7 @@ if(x=="6"){xxx="/2019/06/how-to-choose-erp-software-for-midsize.html"}
 if(x=="7"){xxx="/2019/06/the-biggest-erp-software-companies.html"}
 if(x=="8"){xxx="/2019/06/what-does-erp-software-mean-for-your.html"}
 if(x=="9"){xxx="/2019/03/the-benefits-of-erp-for-small-business.html"}
-if(x=="10"){xxx="/2019/01/which-is-best-erp-software-for-your.html"}a_to_ve[i].href="http://erp-software-services.review"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="10"){xxx="/2019/01/which-is-best-erp-software-for-your.html"}a_to_ve[i].href="http://www.erp-software-services.review"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
